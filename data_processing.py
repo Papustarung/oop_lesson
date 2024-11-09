b9160033ab57ad374313bbccef4b062f2258330e
Amornrit Sirikham <amornrit.s@ku.th>
2024-11-09 23:58:51 +0700

    Modify data_processing.py into OO style, display max and min latitude for city in every country

--- data_processing.py
+++ data_processing.py
@@ -15,96 +15,69 @@ with open(os.path.join(__location__, 'Countries.csv')) as f:
     for r in rows:
         countries.append(dict(r))
 
-# Print the average temperature of all the cities
-print("The average temperature of all the cities:")
-temps = []
-for city in cities:
-    temps.append(float(city['temperature']))
-print(sum(temps) / len(temps))
-print()
+###############################################################
 
-# Print all cities in Italy
-cities_temp = []
-my_country = 'Italy'
-for city in cities:
-    if city['country'] == my_country:
-        cities_temp.append(city['city'])
-print("All the cities in", my_country, ":")
-print(cities_temp)
-print()
+class TableDB:
+    def __init__(self):
+        self.table_database = []
 
-# Print the average temperature for all the cities in Italy
-temps = []
-my_country = 'Italy'
-for city in cities:
-    if city['country'] == my_country:
-        temps.append(float(city['temperature']))
-print("The average temperature of all the cities in", my_country, ":")
-print(sum(temps) / len(temps))
-print()
+    def insert(self, table):
+        self.table_database.append(table)
 
-# Print the max temperature for all the cities in Italy
-temps = []
-my_country = 'Italy'
-for city in cities:
-    if city['country'] == my_country:
-        temps.append(float(city['temperature']))
-print("The max temperature of all the cities in", my_country, ":")
-print(max(temps))
-print()
+    def search(self, table_name):
+        for table in self.table_database:
+            if table.table_name == table_name:
+                return table
 
-# Print the min temperature for all the cities in Italy
-temps = []
-my_country = 'Italy'
-for city in cities:
-    if city['country'] == my_country:
-        temps.append(float(city['temperature']))
-print("The min temperature of all the cities in", my_country, ":")
-print(min(temps))
-print()
+class Table:
+    def __init__(self, table_name, table):
+        self.table_name = table_name
+        self.table = table
+
+    def filter(self, condition):
+        filtered_list = []
+        for item in self.table:
+            if condition(item):
+                filtered_list.append(item)
+        return filtered_list
+
+    def aggregate(self, aggregation_function, aggregation_key, condition=None):
+        value = []
+        filtered_table = self.table if condition is None else [row for row in self.table if condition(row)]
+        for item in filtered_table:
+            value.append(float(item[aggregation_key]))
+        if not value:
+            return None
+        return aggregation_function(value)
+
+    def __str__(self):
+        pass
+
+table_database = TableDB()
+cities_table = Table('cities', cities)
+countries_table = Table('countries', countries)
+table_database.insert(cities_table)
+table_database.insert(countries_table)
 
 
-# Let's write a function to filter out only items that meet the condition
-# Hint: condition will be associated with an anonymous function, e.x., lamdbda x: max(x)
-def filter(condition, dict_list):
-    filtered_list = []
-    for item in dict_list:
-        if condition(item):
-            filtered_list.append(item)
-    return filtered_list
+for country in countries_table.table:
+    max_lat = table_database.search("cities").aggregate(
+        lambda x: max(x),
+        "latitude",
+        lambda x: x['country'] == f"{country["country"]}"
+    )
+    if max_lat is not None:
+        print(f"Maximum latitude of {country['country']} is {max_lat}")
+    else:
+        print(f"No data available for {country['country']}")
 
-
-x = filter(lambda x: float(x['latitude']) >= 60.0, cities)
-for item in x:
-    print(item)
-
-
-# Let's write a function to do aggregation given an aggregation function and an aggregation key
-def aggregate(aggregation_key, aggregation_function, dict_list):
-    temp = []
-    for item in dict_list:
-        temp.append(float(item[aggregation_key]))
-    return aggregation_function(temp)
-
-# Let's write code to
-print()
-# - print the average temperature for all the cities in Italy
-avg_italy_temp = aggregate("temperature", lambda x: sum(x)/len(x), filter(lambda x: x['country'] == "Italy", cities))
-print("The average temperature for all the cities in Italy :")
-print(avg_italy_temp)
-print()
-# - print the average temperature for all the cities in Sweden
-avg_sweden_temp = aggregate("temperature", lambda x: sum(x)/len(x), filter(lambda x: x['country'] == "Sweden", cities))
-print("The average temperature for all the cities in Sweden :")
-print(avg_sweden_temp)
-print()
-# - print the min temperature for all the cities in Italy
-min_italy_temp = aggregate("temperature", lambda x: min(x), filter(lambda x: x['country'] == "Italy", cities))
-print("The min temperature for all the cities in Italy :")
-print(min_italy_temp)
-print()
-# - print the max temperature for all the cities in Sweden
-max_sweden_temp = aggregate("temperature", lambda x: max(x), filter(lambda x: x['country'] == "Sweden", cities))
-print("The max temperature for all the cities in Sweden :")
-print(max_sweden_temp)
-print()
+    min_lat = table_database.search("cities").aggregate(
+        lambda x: min(x),
+        "latitude",
+        lambda x: x['country'] == f"{country["country"]}"
+    )
+    if min_lat is not None:
+        print(f"Minimum latitude of {country['country']} is {min_lat}")
+    else:
+        print(f"No data available for {country['country']}")
+    print("-------------------------------------------")
